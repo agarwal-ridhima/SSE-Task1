9e6fa37930ca76ef336e622e345c25ed68717f74
Ridhima Agarwal <ridhima13aga@gmail.com>
2026-09-25 00:37:40 +0530

    Add string input program

---
+++ basics/02_string_getline.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    getline(cin,s);
+    // getline used to take full string otherwise cin >> s1 >> s2
+    //char can take all
+    cout << s;
+    return 0;
+}
